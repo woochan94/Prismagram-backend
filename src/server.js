5df9a507a17cfab32f40daf9c7588243cd6b161e
정우찬 <vdncks25@naver.com>
2019-05-25 20:32:25 +0900

    CHORE : Create graphql server
    GraphQL 서버 생성 
    - dotenv 설치 및 적용 
    - babelrc : preset-env 적용 
    - babel 관련 모듈 설치, babel-cli 제거

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log("hello");
+import dotenv from "dotenv"; 
+import { GraphQLServer } from "graphql-yoga";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello : () => "Hi"
+    }
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+
+server.start({ port: PORT }, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+);
