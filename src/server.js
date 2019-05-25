9cec912d3783e41051906093c51d5febadb21164
정우찬 <vdncks25@naver.com>
2019-05-25 20:54:36 +0900

    CHORE : Setting up the server
    -여러 typeDefs와 resolvers를 하나로 묶는 방법 적용 
    ( makeExecutableSchema) 
    - sayHello, sayGoodbye api 생성 
    - graphql tool 설치 
    - morgan 설치 및 적용

--- src/server.js
+++ src/server.js
@@ -1,24 +1,17 @@
 import dotenv from "dotenv"; 
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query {
-        hello: String!
-    }
-`;
+const server = new GraphQLServer({ schema });
 
-const resolvers = {
-    Query: {
-        hello : () => "Hi"
-    }
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers })
+// GraphQLServer에는 express 서버가 내장되어 있다. 
+server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () =>
-    console.log(`Server running on http://localhost:${PORT}`)
+    console.log(`✅  Server running on http://localhost:${PORT}`)
 );
