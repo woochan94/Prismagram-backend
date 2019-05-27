495fa3eacda1529ff721dc4c78bac68760a79f31
정우찬 <vdncks25@naver.com>
2019-05-27 22:27:11 +0900

    STYLE : Change code "isAuthenticated" using context
    - server.js에 isAuthenticated import 후 context에 추가 
    - isAuthenticated를 사용해야 하는 resolver 파일에서 import를 하지 않고 context에서 가져오면 됨

--- src/server.js
+++ src/server.js
@@ -4,11 +4,12 @@ import logger from "morgan";
 import schema from "./schema";
 import "./passport"; 
 import { authenticateJwt } from "./passport";
+import { isAuthenticated } from "./middlewares";
 
 const PORT = process.env.PORT || 4000;
 
 // context는 resolver 사이에서 정보를 공유할 때 사용 
-const server = new GraphQLServer({ schema, context: ({request}) => ({request})
+const server = new GraphQLServer({ schema, context: ({request}) => ({request, isAuthenticated})
 });
 
 // GraphQLServer에는 express 서버가 내장되어 있다. 
