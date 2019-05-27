fd8a0d032470d81d52ceef35cd9886e822979288
정우찬 <vdncks25@naver.com>
2019-05-27 22:25:49 +0900

    FEAT : Create editUser resolver
    사용자 정보 수정

---
+++ src/api/User/editUser/editUser.js
@@ -0,0 +1,20 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Mutation: {
+        editUser: async(_, args, { request, isAuthenticated }) => {
+            isAuthenticated(request); 
+            const { username, email, firstName, lastName, bio } = args; 
+            const { user } = request; 
+            // update할때 빈 매개변수를 보내더라도 이전의 데이터를 보존해준다. 
+            return prisma.updateUser({
+                where: {
+                    id: user.id
+                }, 
+                data: {
+                    username, email, firstName, lastName, bio
+                }
+            });
+        }
+    }
+}
