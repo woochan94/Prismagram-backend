c23ecd00bfcda57ff58f8f37e0a60aa1b3d78037
정우찬 <vdncks25@naver.com>
2019-05-27 21:59:33 +0900

    STYLE : Change follow/unfollow code → toggleFollow
    follow/unfollow로 나뉘어 있던 코드를 toggleLike처럼 
    toggleFollow라는 하나의 폴더에서 follow/unfollow할 수 있는 코드 작성

---
+++ src/api/User/toggleFollow/toggleFollow.js
@@ -0,0 +1,52 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Mutation: {
+        toggleFollow: async(_, args, { request }) => {
+            isAuthenticated(request); 
+            const { id } = args; 
+            const { user } = request; 
+
+            try {
+                const existingFollow = await prisma.$exists.user({
+                    AND: [
+                        { id: user.id },
+                        { following_some: { id }}
+                    ]
+                }); 
+                if(existingFollow) {
+                    await prisma.updateUser({
+                        where: {
+                            id: user.id
+                        }, 
+                        data: {
+                            following: {
+                                disconnect: {
+                                    id
+                                }
+                            }
+                        }
+                    });
+                    return true; 
+                } else {
+                    await prisma.updateUser({
+                        where: {
+                            id: user.id
+                        }, 
+                        data: {
+                            following: {
+                                connect: {
+                                    id
+                                }
+                            }
+                        }
+                    })
+                    return true;
+                }
+            } catch {
+                return false;
+            }
+        }
+    }
+}
