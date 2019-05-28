2111746d968f67b9f271fda033261594ec1c28cf
정우찬 <vdncks25@naver.com>
2019-05-28 16:29:45 +0900

    STYLE : Refactoring seeFullPost using fragment
    fragment를 사용하여 seeFullPost 리팩토링 
    - computed.js를 폴더별로 만듬 
    - FULL_POST_FRAGMENT를 통해 fragment 재사용

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -27,23 +27,5 @@ export default {
             const { id: ParentId } = parent; 
             return user.id === ParentId; 
         }
-    },
-    Post: {
-        isLiked: async (parent, __, { request }) => {
-            const { user } = request; 
-            const { id } = parent; 
-            // like 테이블? 에서 parent로 가져온 postId와 저장된 postId를 비교하고
-            // request에서 가져온 userid와 저장된 user의 id를 비교하여 좋아요 유무 판별 
-            return prisma.$exists.like({
-                AND: [
-                    { user: {
-                        id: user.id
-                    }}, 
-                    { post: {
-                        id
-                    }}
-                ]
-            }); 
-        }
     }
 };
