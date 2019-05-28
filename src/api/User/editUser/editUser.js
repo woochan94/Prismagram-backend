e3bda6a9d97f83e725ad46ba5bb857a3d49018e5
정우찬 <vdncks25@naver.com>
2019-05-28 21:25:20 +0900

    STYLE : Add avatar
    datamodel에 avatar 추가

--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -4,7 +4,7 @@ export default {
     Mutation: {
         editUser: async(_, args, { request, isAuthenticated }) => {
             isAuthenticated(request); 
-            const { username, email, firstName, lastName, bio } = args; 
+            const { username, email, firstName, lastName, bio, avatar } = args; 
             const { user } = request; 
             // update할때 빈 매개변수를 보내더라도 이전의 데이터를 보존해준다. 
             return prisma.updateUser({
@@ -12,7 +12,7 @@ export default {
                     id: user.id
                 }, 
                 data: {
-                    username, email, firstName, lastName, bio
+                    username, email, firstName, lastName, bio, avatar
                 }
             });
         }
