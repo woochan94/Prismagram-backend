c8dfb201c0286bb1084f5defd14255fb1b6359ba
정우찬 <vdncks25@naver.com>
2019-06-05 15:04:43 +0900

    REVIEW : #1.1 Creating GraphQL Server

--- review/src/server.js
+++ review/src/server.js
@@ -1,1 +1,20 @@
-console.log("hello");
+import dotenv from "dotenv"; 
+import {GraphQLServer} from "graphql-yoga"
+
+dotenv.config(); 
+
+// graphql-yoga Quickstart 
+const typeDefs = `
+    type Query {
+        hello : String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => "Hi"
+    }
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers }); 
+server.start(() => console.log('Server is running on localhost:4000'))
