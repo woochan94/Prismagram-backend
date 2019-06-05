7cd191b08f559c870c5aef26ec1c144d7041572d
정우찬 <vdncks25@naver.com>
2019-06-05 22:11:56 +0900

    REVIEW : #3.3 sendMail Function with Nodemailer

--- review/src/server.js
+++ review/src/server.js
@@ -1,9 +1,7 @@
-import dotenv from "dotenv"; 
+import "./env";
 import {GraphQLServer} from "graphql-yoga"
 import logger from "morgan";
 import schema from './schema';
-
-dotenv.config(); 
 
 // GraphQLServer에는 express 서버가 내장되어 있다. 
 // 그러므로 express 모듈을 따로 설치할 필요 없이 express에 관한 함수를 사용할 수 있다. 
--- review/src/utils.js
+++ review/src/utils.js
@@ -1,6 +1,29 @@
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport"; 
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
+
+export const sendMail = (email) => {
+  const options = {
+      auth: {
+          api_user: process.env.SENDGRID_USERNAME,
+          api_key: process.env.SENDGRID_PASSWORD
+      }
+  }
+  const client = nodemailer.createTransport(sgTransport(options)); 
+  return client.sendMail(email);
+}; 
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+      from: "woochan@prismagram.com", 
+      to: address, 
+      subject: "Login Secret for Prismagram  🔒",
+      html: `Hello! Your login secret is <strong>${secret}</strong>.<br/> Copy paste on the app/website to Log in`
+  };
+  return sendMail(email);
+}
