6c68746b750bf53acfb0003aed1914cb66e7ff5e
정우찬 <vdncks25@naver.com>
2019-06-06 22:01:21 +0900

    REVIEW : #3.15~3.16 isSelf and isFollowing Fields

--- review/src/api/User/User.js
+++ review/src/api/User/User.js
@@ -5,6 +5,31 @@ export default {
         posts: ({ id }) => prisma.user({ id }).posts(), 
         fullName: (parent) => {
             return `${parent.firstName} ${parent.lastName}`;
+        },
+        isSelf: (parent, __, { request }) => {
+            const { user } = request; 
+            const { id: parentId } = parent; 
+            return user.id === parentId
+        },
+        isFollowing: async(parent, _, { request }) => {
+            const { user } = request; 
+            const { id:parentId } = parent; 
+            try {
+                return prisma.$exists.user({
+                    AND: [
+                        {
+                            id: user.id 
+                        }, 
+                        {
+                            following_some: {
+                                id: parentId
+                            }
+                        }
+                    ]
+                })
+            } catch {
+                return false; 
+            }
         }
     }
 }
