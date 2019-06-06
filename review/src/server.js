38db923ef6cb21646abcec1bfa7a0c861934fbf5
정우찬 <vdncks25@naver.com>
2019-06-06 16:45:30 +0900

    REVIEW : #3.7 toggleLike Resolver

--- review/src/server.js
+++ review/src/server.js
@@ -4,12 +4,13 @@ import logger from "morgan";
 import schema from './schema';
 import "./passport"; 
 import { authenticateJwt } from "./passport";
+import { isAuthenticated } from './middlewares';
 
 // GraphQLServer에는 express 서버가 내장되어 있다. 
 // 그러므로 express 모듈을 따로 설치할 필요 없이 express에 관한 함수를 사용할 수 있다. 
 const server = new GraphQLServer({
     schema,
-    context: ({ request }) => ({ request })
+    context: ({ request }) => ({ request, isAuthenticated })
   });
 
 server.express.use(logger("dev"));
