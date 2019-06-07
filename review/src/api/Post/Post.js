6960abaafe3ab074a97c3cc0ae3cea5df9ce997f
정우찬 <vdncks25@naver.com>
2019-06-07 22:50:48 +0900

    REVIEW : #3.30 Kill The Fragments

--- review/src/api/Post/Post.js
+++ review/src/api/Post/Post.js
@@ -1,36 +1,42 @@
-import { prisma } from "../../../generated/prisma-client"; 
+import { prisma } from "../../../generated/prisma-client";
 
 export default {
-    Post: {
-        user: ({ id }) => prisma.post({ id }).user(), 
-        comments: ({ id }) => prisma.post({ id }).comments(), 
-        likeCount: (parent) => prisma.likesConnection({
-            where: {
-                post: { id: parent.id }
+  Post: {
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
+    likes: ({ id }) => prisma.post({ id }).likes(),
+    isLiked: (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id
             }
-        }).aggregate().count(),
-        isLiked: (parent, _, { request }) => {
-            const { user } = request; 
-            const { id: parentId } = parent; 
-            try {
-                return prisma.$exists.like({
-                    AND: [
-                        {
-                            user: {
-                                id: user.id
-                            }
-                        }, 
-                        {
-                            post: {
-                                id: parentId
-                            }   
-                        }
-                    ]
-                })
-            } catch {
-                return false; 
+          },
+          {
+            post: {
+              id
             }
-        },
-        files: ({ id }) => prisma.post({ id }).files()
-    }
-}
+          }
+        ]
+      });
+    },
+    likeCount: parent =>
+      prisma
+        .likesConnection({
+          where: { post: { id: parent.id } }
+        })
+        .aggregate()
+        .count(),
+    commentCount: parent =>
+      prisma
+        .commentsConnection({
+          where: { post: { id: parent.id } }
+        })
+        .aggregate()
+        .count()
+  }
+};
